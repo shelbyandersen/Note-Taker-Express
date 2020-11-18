1bbfd0ceefde9a627c35cf110dc65fffa2397536
Shelby <shelbyrobison14@gmail.com>
2020-11-18 14:19:53 -0500

    required fs in app route file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,11 +1,22 @@
+// Require
+const fs = require("fs");
+
 // Create function
 module.exports = (app) => {
-    const notes = [];
+    const notes = [
+        {
+            "title": "Test Title",
+            "text": "Test text",
+            "id": "001"
+        }
+    ];
 
     app.get("api/notes", (req, res) => {
-        res.json.notes
+        res.json(data)
     })
 
+    // POST: Read JSON file and Add it
+    // POST: Rewrite JSON file
     app.post("/api/notes", (req, res) => {
         notes.push(req.body);
         res.json(notes)
